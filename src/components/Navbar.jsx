35c55a17817d10a3b4a71690c4ee39ba4234dca2
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-07-28 18:06:00 +0500

    current code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,21 @@
 import { NavLink } from 'react-router-dom'
+import reactLogo from '../assets/images/react-icon_2.png'
 
 function Navbar() {
     return (
         <nav className='navbar navbar-expand-lg  bg-warning-subtle'>
             <div className='container-fluid '>
-                <a className='navbar-brand'>DreamTeam</a>
-                <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-bs-toggle='collapse'
-                    data-bs-target='#navbarSupportedContent'
-                    aria-controls='navbarSupportedContent'
-                    aria-expanded='false'
-                    aria-label='Toggle navigation'
-                >
-                    <span className='navbar-toggler-icon'></span>
-                </button>
                 <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                    <img
+                        src={reactLogo}
+                        className='img-fluid'
+                        alt='reactLogo'
+                        style={{
+                            width: '80px',
+                            height: '70px',
+                            marginRight: '15px'
+                        }}
+                    />
                     <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
                         <li className='nav-item'>
                             <NavLink to='/' className='nav-link'>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,5 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
+import result from '../assets/images/result.jpg'
+
 function MainPage() {
-    return <h1>MainPage</h1>
+    return (
+        <div className='container-xl mt-5'>
+            <img src={result} alt='resultSchool' style={{ width: '300px', height: '300px' }} />
+            <p className=' mt-5'>
+                Добро пожаловать на страницу группы 91 курса Junior Frontend Developer в
+                ResultSchool! Здесь собралась команда единомышленников и, в первую очередь,
+                увлёченных web разработкой людей. В рамках данного практического задания мы
+                постарались воплотить в проекте те знания, которые кропотливо получали в течение
+                многих месяцев. Проходи в раздел "Команда" чтобы познакомиться с нами поближе :)
+            </p>
+            <br />
+            <br />
+            <p>В данном проекте были использованы следующие технологии:</p>
+            <div>
+                <button className='btn btn-danger m-2'> HTML </button>
+                <button className='btn btn-primary m-2'> CSS </button>
+                <button className='btn btn-info m-2'>React </button>
+                <button className='btn btn-success m-2'>React Router v.6 </button>
+                <button className='btn btn-warning m-2'> Bootstrap </button>
+            </div>
+        </div>
+    )
 }
 
 export default MainPage
