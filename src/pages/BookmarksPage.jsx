e19fc634aa23e998f9ec76f92221c811a55e0237
Andrey Nazarenko <skiminok86@gmail.com>
2023-07-30 02:30:00 +0300

    add favorites

--- src/pages/BookmarksPage.jsx
+++ src/pages/BookmarksPage.jsx
@@ -1,29 +1,40 @@
-import { useEffect, useState } from 'react'
-import API from '../mockData/members.api'
-import MemberCard from '../components/MemberCard'
+import { useEffect, useState } from "react"
+import API from "../mockData/members.api"
+import MemberCard from "../components/MemberCard"
+import favorites from "../utils/favorites"
 
 function BookmarksPage() {
     const [team, setTeam] = useState([])
+    const [favoriteList, setFavoriteList] = useState([])
+    const memberIds = favorites.get()
 
     useEffect(() => {
-        API.fetchAll().then(data => setTeam(data))
+        API.fetchAll().then((data) => setTeam(data))
     }, [])
 
-    if (team.length > 0) {
-        return (
-            <div className='container-xl mt-5'>
-                <div className='memberCards mt-5'>
-                    {team.map(member =>
-                        localStorage.getItem(member._id) ? (
+    useEffect(() => {
+        setFavoriteList(team.filter((member) => memberIds.includes(member._id)))
+    }, [team])
+
+    if (team) {
+        if (favoriteList.length > 0) {
+            return (
+                <div className="container-xl mt-5">
+                    <div className="memberCards mt-5">
+                        {favoriteList.map((member) => (
                             <MemberCard key={member._id} {...member} />
-                        ) : (
-                            <h1 key={member._id}>Пока в избранном никого нет...</h1>
-                        )
-                    )}
+                        ))}
+                    </div>
                 </div>
+            )
+        }
+        return (
+            <div class="alert alert-warning" role="alert">
+                Пока в избранном никого нет...
             </div>
         )
     }
+    return "Loading..."
 }
 
 export default BookmarksPage
