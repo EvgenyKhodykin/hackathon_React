cc936e93680ecca8866e902a1bfc82d3c9ea633c
Evgeny Khodykin <evgeny.khodykin@yandex.ru>
2023-07-30 17:36:58 +0500

    new photos

---
+++ src/components/Progress.jsx
@@ -0,0 +1,61 @@
+function Progress() {
+    return (
+        <div className='container'>
+            <div className='row justify-content-center'>
+                <div className='col-3'>
+                    <p className='technologies'>HTML</p>
+                </div>
+                <div className='col-9'>
+                    <div className='progress'>
+                        <div
+                            className='progress-bar bg-danger'
+                            role='progressbar'
+                            style={{ width: '75%' }}
+                            aria-valuenow='75'
+                            aria-valuemin='0'
+                            aria-valuemax='100'
+                        >
+                            75%
+                        </div>
+                    </div>
+                </div>
+                <div className='col-3'>
+                    <p className='technologies'>CSS</p>
+                </div>
+                <div className='col-9'>
+                    <div className='progress'>
+                        <div
+                            className='progress-bar bg-primary'
+                            role='progressbar'
+                            style={{ width: '50%' }}
+                            aria-valuenow='50'
+                            aria-valuemin='0'
+                            aria-valuemax='100'
+                        >
+                            50%
+                        </div>
+                    </div>
+                </div>
+                <div className='col-3'>
+                    <p className='technologies'>React</p>
+                </div>
+                <div className='col-9'>
+                    <div className='progress'>
+                        <div
+                            className='progress-bar bg-info'
+                            role='progressbar'
+                            style={{ width: '25%' }}
+                            aria-valuenow='25'
+                            aria-valuemin='0'
+                            aria-valuemax='100'
+                        >
+                            25%
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Progress
--- src/pages/MemberPage.jsx
+++ src/pages/MemberPage.jsx
@@ -1,33 +1,34 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import API from "../mockData/members.api"
-import Slider from "../components/Slider"
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import API from '../mockData/members.api'
+import Slider from '../components/Slider'
 
 function MemberPage() {
     const { userId } = useParams()
     const [user, setUser] = useState()
 
     useEffect(() => {
-        API.getById(userId).then((data) => setUser(data))
+        API.getById(userId).then(data => setUser(data))
     }, [])
 
     if (user) {
         return (
             <>
-                <div className="container mt-5">
-                    <div className="row justify-content-center">
-                        <div className="col-12">
+                <div className='container mt-5'>
+                    <div className='row justify-content-center'>
+                        <div className='col-12'>
                             <div>
                                 <Slider userId={userId} />
                             </div>
                         </div>
-                        <div className="col-8"></div>
+                        <div className='col-8'></div>
                     </div>
                 </div>
+                {/* Techs */}
             </>
         )
     }
-    return "Loading..."
+    return 'Loading...'
 }
 
 export default MemberPage
