fb3dd69092594bd56dd0c06b685fd12a1708552b
Andrey Nazarenko <skiminok86@gmail.com>
2023-08-01 00:08:25 +0300

    change loading

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,45 +1,47 @@
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from "react-router-dom"
 
 function Breadcrumbs() {
     const location = useLocation()
 
     return (
-        <nav aria-label='breadcrumb' className='sticky-breadcrumbs'>
-            <ol className='breadcrumb'>
-                <li className='breadcrumb-item'>
-                    <NavLink to='/' className='breadcrumb-item' aria-current='page'>
-                        Главная
-                    </NavLink>
-                </li>
-                {location.pathname.includes('team') && (
-                    <li className='breadcrumb-item'>
-                        <NavLink to='/team' className='breadcrumb-item' aria-current='page'>
-                            Команда
+        <nav aria-label="breadcrumb" className="sticky-breadcrumbs">
+            <div className="container-fluid">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <NavLink to="/" className="breadcrumb-item" aria-current="page">
+                            Главная
                         </NavLink>
                     </li>
-                )}
-                {location.pathname.includes('team/e33882167e2d43a0971c') && (
-                    <li className='breadcrumb-item'>
-                        <NavLink to='/team' className='breadcrumb-item' aria-current='page'>
-                            Евгений Ходыкин
-                        </NavLink>
-                    </li>
-                )}
-                {location.pathname.includes('team/78e6b4902884fb130fd7') && (
-                    <li className='breadcrumb-item'>
-                        <NavLink to='/team' className='breadcrumb-item' aria-current='page'>
-                            Андрей Назаренко
-                        </NavLink>
-                    </li>
-                )}
-                {location.pathname.includes('bookmarks') && (
-                    <li className='breadcrumb-item'>
-                        <NavLink to='/bookmarks' className='breadcrumb-item' aria-current='page'>
-                            Избранное
-                        </NavLink>
-                    </li>
-                )}
-            </ol>
+                    {location.pathname.includes("team") && (
+                        <li className="breadcrumb-item">
+                            <NavLink to="/team" className="breadcrumb-item" aria-current="page">
+                                Команда
+                            </NavLink>
+                        </li>
+                    )}
+                    {location.pathname.includes("team/e33882167e2d43a0971c") && (
+                        <li className="breadcrumb-item">
+                            <NavLink to="/team" className="breadcrumb-item" aria-current="page">
+                                Евгений Ходыкин
+                            </NavLink>
+                        </li>
+                    )}
+                    {location.pathname.includes("team/78e6b4902884fb130fd7") && (
+                        <li className="breadcrumb-item">
+                            <NavLink to="/team" className="breadcrumb-item" aria-current="page">
+                                Андрей Назаренко
+                            </NavLink>
+                        </li>
+                    )}
+                    {location.pathname.includes("bookmarks") && (
+                        <li className="breadcrumb-item">
+                            <NavLink to="/bookmarks" className="breadcrumb-item" aria-current="page">
+                                Избранное
+                            </NavLink>
+                        </li>
+                    )}
+                </ol>
+            </div>
         </nav>
     )
 }
